128d4626229dcd3b222309412649a5900050f771
Lari Hotari <lari@hotari.net>
2021-08-27 18:08:53 +0300

    Add process which sends telemetry events when the value goes over a threshold and another one when it goes below

---
+++ src/intTest/java/com/github/lhotari/reactive/pulsar/showcase/AlarmProcessorTest.java
@@ -0,0 +1,153 @@
+package com.github.lhotari.reactive.pulsar.showcase;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.lhotari.reactive.pulsar.adapter.MessageSpec;
+import com.github.lhotari.reactive.pulsar.adapter.ReactiveMessageSender;
+import com.github.lhotari.reactive.pulsar.adapter.ReactivePulsarClient;
+import com.github.lhotari.reactive.pulsar.spring.PulsarTopicNameResolver;
+import com.github.lhotari.reactive.pulsar.spring.test.SingletonPulsarContainer;
+import com.github.tomakehurst.wiremock.common.FileSource;
+import com.github.tomakehurst.wiremock.extension.Parameters;
+import com.github.tomakehurst.wiremock.extension.ResponseTransformer;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.Response;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.client.api.Schema;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.cloud.contract.wiremock.WireMockConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import reactor.core.publisher.Flux;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = "alarmwebhook.url=http://localhost:${wiremock.server.port}/webhook")
+@ContextConfiguration(initializers = SingletonPulsarContainer.ContextInitializer.class)
+@AutoConfigureWireMock(port = 0)
+@Slf4j
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+class AlarmProcessorTest {
+    @Autowired
+    ReactivePulsarClient reactivePulsarClient;
+
+    @Autowired
+    PulsarTopicNameResolver topicNameResolver;
+
+    @Autowired
+    WebhookResponseHolder webhookResponseHolder;
+
+    @Value
+    static class WebhookResponseHolder {
+        private final BlockingQueue<TelemetryEvent> responses = new ArrayBlockingQueue<>(100);
+
+        public void add(TelemetryEvent telemetryEvent) {
+            responses.add(telemetryEvent);
+        }
+
+        public TelemetryEvent waitForResponse() throws InterruptedException {
+            return responses.poll(5, TimeUnit.SECONDS);
+        }
+
+        public List<TelemetryEvent> waitForResponses(int numberOfResponses) throws InterruptedException {
+            List<TelemetryEvent> results = new ArrayList<>(numberOfResponses);
+            while (results.size() < numberOfResponses) {
+                TelemetryEvent telemetryEvent = waitForResponse();
+                if (telemetryEvent == null) {
+                    break;
+                }
+                results.add(telemetryEvent);
+            }
+            return results;
+        }
+    }
+
+    static class WebhookResponseTransformer extends ResponseTransformer {
+        static final String WEBHOOK_TRANSFORMER_NAME = "webhook";
+        private final ObjectMapper objectMapper;
+        private final WebhookResponseHolder webhookResponseHolder;
+
+        public WebhookResponseTransformer(ObjectMapper objectMapper, WebhookResponseHolder webhookResponseHolder) {
+            this.objectMapper = objectMapper;
+            this.webhookResponseHolder = webhookResponseHolder;
+        }
+
+        @Override
+        public Response transform(Request request, Response response, FileSource files,
+                                  Parameters parameters) {
+            try {
+                TelemetryEvent telemetryEvent =
+                        objectMapper.readValue(request.getBodyAsString(), TelemetryEvent.class);
+                webhookResponseHolder.add(telemetryEvent);
+                return Response.response().status(200).build();
+            } catch (JsonProcessingException e) {
+                log.error("Failed to deserialize request body to TelemetryEvent", e);
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        @Override
+        public String getName() {
+            return WEBHOOK_TRANSFORMER_NAME;
+        }
+
+        @Override
+        public boolean applyGlobally() {
+            return false;
+        }
+    }
+
+    @TestConfiguration
+    static class CustomConfig {
+        @Bean
+        WebhookResponseHolder webhookResponseHolder() {
+            return new WebhookResponseHolder();
+        }
+
+        @Bean
+        WireMockConfigurationCustomizer optionsCustomizer(WebhookResponseHolder webhookResponseHolder,
+                                                          ObjectMapper objectMapper) {
+            return config -> {
+                config.disableRequestJournal();
+                config.extensions(new WebhookResponseTransformer(objectMapper, webhookResponseHolder));
+            };
+        }
+    }
+
+    @Test
+    void shouldDeliverEventWhenValueGoesOverThresholdAndWhenItGoesBackToNormal() throws InterruptedException {
+        // given
+        stubFor(post(urlEqualTo("/webhook")).willReturn(aResponse().withTransformers(
+                WebhookResponseTransformer.WEBHOOK_TRANSFORMER_NAME)));
+        ReactiveMessageSender<TelemetryEvent> messageSender = reactivePulsarClient
+                .messageSender(Schema.JSON(TelemetryEvent.class))
+                .topic(topicNameResolver.resolveTopicName(TelemetryProcessor.TELEMETRY_MEDIAN_TOPIC_NAME))
+                .create();
+
+        // when telemetry events are sent
+        messageSender.sendMessages(Flux.just(1.0d, 2.0d, 81.0d, 120.0d, 66.0d, 75.0d, 73.0d, 82.0d)
+                        .map(value -> new TelemetryEvent("device1/sensor1", value))
+                        .map(MessageSpec::of))
+                .blockLast();
+
+        // then
+        List<TelemetryEvent> receivedAlarmEvents = webhookResponseHolder.waitForResponses(3);
+        assertThat(receivedAlarmEvents).extracting(TelemetryEvent::getV).containsExactly(81.0d, 66.0d, 82.0d);
+    }
+}
---
+++ src/main/java/com/github/lhotari/reactive/pulsar/showcase/AlarmProcessor.java
@@ -0,0 +1,127 @@
+package com.github.lhotari.reactive.pulsar.showcase;
+
+import com.github.lhotari.reactive.pulsar.adapter.MessageResult;
+import com.github.lhotari.reactive.pulsar.adapter.ReactiveMessageHandler;
+import com.github.lhotari.reactive.pulsar.adapter.ReactiveMessageHandlerBuilder;
+import com.github.lhotari.reactive.pulsar.adapter.ReactivePulsarClient;
+import com.github.lhotari.reactive.pulsar.spring.AbstractReactiveMessageListenerContainer;
+import com.github.lhotari.reactive.pulsar.spring.PulsarTopicNameResolver;
+import java.time.Duration;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.client.api.ConsumerBuilder;
+import org.apache.pulsar.client.api.DeadLetterPolicy;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
+import org.apache.pulsar.client.api.SubscriptionType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+@Component
+@Slf4j
+public class AlarmProcessor extends AbstractReactiveMessageListenerContainer {
+    public static final String ALARMPROCESSOR_DLQ_TOPIC_NAME = "alarmprocessor.dlq";
+    private final ReactivePulsarClient reactivePulsarClient;
+    private final Schema<TelemetryEvent> schema;
+    private final PulsarTopicNameResolver topicNameResolver;
+    private final ConcurrentHashMap<String, TelemetryLastSent> lastSentState = new ConcurrentHashMap<>();
+    private final WebClient webhookWebclient;
+    private final Double ALARM_THRESHOLD = 80.0d;
+
+    @Value
+    private static class TelemetryLastSent {
+        MessageId messageId;
+        TelemetryEvent telemetryEvent;
+    }
+
+    @Autowired
+    public AlarmProcessor(ReactivePulsarClient reactivePulsarClient,
+                          PulsarTopicNameResolver topicNameResolver,
+                          WebClient.Builder webClientBuilder,
+                          @org.springframework.beans.factory.annotation.Value(
+                                  "${alarmwebhook.url:http://localhost:8082/webhook}")
+                                  String alarmWebhookUrl) {
+        this.topicNameResolver = topicNameResolver;
+        schema = Schema.JSON(TelemetryEvent.class);
+        this.reactivePulsarClient = reactivePulsarClient;
+        this.webhookWebclient = webClientBuilder
+                .baseUrl(alarmWebhookUrl)
+                .build();
+    }
+
+    @Override
+    protected ReactiveMessageHandler createReactiveMessageHandler() {
+        log.info("Starting to consume messages");
+        return ReactiveMessageHandlerBuilder
+                .builder(reactivePulsarClient
+                        .messageConsumer(schema)
+                        .consumerConfigurer(this::configureConsumer)
+                        .create())
+                .streamingMessageHandler(this::consumeMessages)
+                .build();
+    }
+
+    private void configureConsumer(ConsumerBuilder<TelemetryEvent> consumerBuilder) {
+        consumerBuilder
+                .topic(topicNameResolver.resolveTopicName(TelemetryProcessor.TELEMETRY_MEDIAN_TOPIC_NAME))
+                .subscriptionType(SubscriptionType.Key_Shared)
+                .subscriptionName(getClass().getSimpleName())
+                .subscriptionInitialPosition(SubscriptionInitialPosition.Earliest)
+                .negativeAckRedeliveryDelay(10, TimeUnit.SECONDS)
+                .deadLetterPolicy(DeadLetterPolicy.builder()
+                        .deadLetterTopic(topicNameResolver.resolveTopicName(ALARMPROCESSOR_DLQ_TOPIC_NAME))
+                        .maxRedeliverCount(3)
+                        .build());
+    }
+
+    private Flux<MessageResult<Void>> consumeMessages(Flux<Message<TelemetryEvent>> messageFlux) {
+        return messageFlux
+                .delayUntil(telemetryEventMessage -> {
+                    if (hasLastSentStateChanged(telemetryEventMessage)) {
+                        return sendStateToWebhook(telemetryEventMessage);
+                    } else {
+                        return Mono.empty();
+                    }
+                })
+                .map(AlarmProcessor::acknowledgeMessage);
+    }
+
+    private Mono<Void> sendStateToWebhook(Message<TelemetryEvent> telemetryEventMessage) {
+        return webhookWebclient.post()
+                .bodyValue(telemetryEventMessage.getValue())
+                .retrieve()
+                .toBodilessEntity()
+                .then(updateLastSentState(telemetryEventMessage))
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(1)));
+    }
+
+    private boolean hasLastSentStateChanged(Message<TelemetryEvent> telemetryEventMessage) {
+        TelemetryEvent telemetryEvent = telemetryEventMessage.getValue();
+        TelemetryLastSent lastSent = lastSentState.get(telemetryEvent.getN());
+        return (lastSent == null && telemetryEvent.getV() > ALARM_THRESHOLD)
+                || (lastSent != null
+                && lastSent.messageId.compareTo(telemetryEventMessage.getMessageId()) < 0
+                && ALARM_THRESHOLD.compareTo(lastSent.getTelemetryEvent().getV()) !=
+                ALARM_THRESHOLD.compareTo(telemetryEvent.getV()));
+    }
+
+    private Mono<Void> updateLastSentState(Message<TelemetryEvent> telemetryEventMessage) {
+        return Mono.fromRunnable(() -> {
+            TelemetryEvent telemetryEvent = telemetryEventMessage.getValue();
+            lastSentState.put(telemetryEvent.getN(),
+                    new TelemetryLastSent(telemetryEventMessage.getMessageId(), telemetryEvent));
+        });
+    }
+
+    private static MessageResult<Void> acknowledgeMessage(Message<TelemetryEvent> telemetryEventMessage) {
+        return MessageResult.acknowledge(telemetryEventMessage.getMessageId());
+    }
+}
