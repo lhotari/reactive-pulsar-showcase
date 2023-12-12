10aa7f00016c29c87a3b27f6a3140edae7fa3150
Lari Hotari <lhotari@users.noreply.github.com>
2023-12-12 14:10:18 +0200

    Remove unused imports

--- src/main/java/com/github/lhotari/reactive/pulsar/showcase/ReactivePulsarShowcaseApplication.java
+++ src/main/java/com/github/lhotari/reactive/pulsar/showcase/ReactivePulsarShowcaseApplication.java
@@ -1,9 +1,7 @@
 package com.github.lhotari.reactive.pulsar.showcase;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ReactivePulsarShowcaseApplication {
