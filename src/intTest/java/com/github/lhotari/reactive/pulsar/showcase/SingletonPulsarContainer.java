eff0fd577f0dbbaeee8546de3508db67d7920191
Lari Hotari <lhotari@users.noreply.github.com>
2024-10-23 15:52:32 +0300

    Upgrade to use Pulsar 4.0.0 client

--- src/intTest/java/com/github/lhotari/reactive/pulsar/showcase/SingletonPulsarContainer.java
+++ src/intTest/java/com/github/lhotari/reactive/pulsar/showcase/SingletonPulsarContainer.java
@@ -42,7 +42,7 @@ public class SingletonPulsarContainer {
 
     private static final String PULSAR_CONTAINER_IMAGE = System
         .getenv()
-        .getOrDefault("PULSAR_CONTAINER_IMAGE", "apachepulsar/pulsar:3.2.3");
+        .getOrDefault("PULSAR_CONTAINER_IMAGE", "apachepulsar/pulsar:4.0.0");
     public static SingletonPulsarContainer INSTANCE = new SingletonPulsarContainer();
 
     private final PulsarContainer pulsarContainer = new PulsarContainer(
